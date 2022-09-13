5ce8284e6ee1eee08f606824e6fe2f6a1b22dbb8
Michael J Ward <ward.michael.j@gmail.com>
2022-09-13 10:01:41 -0400

    remove depcreated function IntoIter::new

--- openfix-messages/tests/it/helpers.rs
+++ openfix-messages/tests/it/helpers.rs
@@ -1,6 +1,5 @@
 
 
-use std::array::IntoIter;
 use std::collections::HashMap;
 use std::iter::FromIterator;
 
@@ -22,33 +21,33 @@ macro_rules! split_non_static {
 fn test_split_message_items_std_payload() {
     assert_eq!(
         split_non_static!(b""),
-        HashMap::from_iter(IntoIter::new([]))
+        HashMap::from_iter([])
     );
     assert_eq!(
         split_non_static!(b"\x01"),
-        HashMap::from_iter(IntoIter::new([]))
+        HashMap::from_iter([])
     );
     assert_eq!(
         split_non_static!(b"\x01\x01\x01\x01"),
-        HashMap::from_iter(IntoIter::new([]))
+        HashMap::from_iter([])
     );
     assert_eq!(
         split_non_static!(b"5=foo"),
-        HashMap::from_iter(IntoIter::new([(5, b"foo".to_vec())]))
+        HashMap::from_iter([(5, b"foo".to_vec())])
     );
     assert_eq!(
         split_non_static!(b"5=foo\x012631=bar"),
-        HashMap::from_iter(IntoIter::new([
+        HashMap::from_iter([
             (5, b"foo".to_vec()),
             (2631, b"bar".to_vec())
-        ]))
+        ])
     );
     assert_eq!(
         split_non_static!(b"\x01\x01\x015=foo\x012631=bar\x01\x01\x01"),
-        HashMap::from_iter(IntoIter::new([
+        HashMap::from_iter([
             (5, b"foo".to_vec()),
             (2631, b"bar".to_vec())
-        ]))
+        ])
     );
 }
 
@@ -56,14 +55,14 @@ fn test_split_message_items_std_payload() {
 fn test_split_message_items_weird_payload() {
     assert_eq!(
         split_non_static!(b"5="),
-        HashMap::from_iter(IntoIter::new([(5, b"".to_vec())]))
+        HashMap::from_iter([(5, b"".to_vec())])
     );
     assert_eq!(
         split_non_static!(b"foo=bar"),
-        HashMap::from_iter(IntoIter::new([]))
+        HashMap::from_iter([])
     );
     assert_eq!(
         split_non_static!(b"foobar"),
-        HashMap::from_iter(IntoIter::new([]))
+        HashMap::from_iter([])
     );
 }
